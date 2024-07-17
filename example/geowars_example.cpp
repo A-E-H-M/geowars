45d5aa0f7f387afe33e35d2325d1d2adb339197a
alexandria <alexandria.hmann@gmail.com>
2024-07-17 18:42:09 -0500

    Renamed and moved example

---
+++ example/geowars_example.cpp
@@ -0,0 +1,14 @@
+// COMP4300 - Game Programming - Assignment 2 - Geometry Wars
+
+//#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "vec2.h"
+#include "entity.h"
+
+#include "game.h"
+
+int main ()
+{
+	Game g("config.txt");
+	g.run();
+}
