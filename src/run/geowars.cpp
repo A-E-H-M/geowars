7f5002027ee8c7ac47b94bbdd62fe05bd1fa624f
alexandria <alexandria.hmann@gmail.com>
2024-09-05 11:14:48 -0500

    Restructured files

--- src/run/geowars.cpp
+++ src/run/geowars.cpp
@@ -1,6 +1,3 @@
-// COMP4300 - Game Programming - Assignment 2 - Geometry Wars
-
-//#include <SFML/Graphics.hpp>
 #include <iostream>
 #include "../include/geowars/vec2.h"
 #include "../include/geowars/entity.h"
