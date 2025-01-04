9cd23888236207e538d726309a8d38e769fd546d
ahm <alexandria.hmann@gmail.com>
2025-01-04 15:51:31 -0600

    Updated test suite

---
+++ tests/components_geowars_test.cpp
@@ -0,0 +1,9 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <vector>
+
+#include "geowars/components.hpp"
+
+TEST_CASE( "Initialize component", "[component]" ) {
+	//
+}
---
+++ tests/terrain_geowars_test.cpp
@@ -0,0 +1,9 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <vector>
+
+#include "geowars/terrain.hpp"
+
+TEST_CASE( "Initialize terrain", "[terrain]" ) {
+	//
+}
