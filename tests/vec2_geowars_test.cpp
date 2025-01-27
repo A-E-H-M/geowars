46fc1b73c3404b0381b45d5c28d2b877448b76ee
alexandria <alexandria.hmann@gmail.com>
2025-01-27 16:44:38 -0600

    Added dependency & fixed compile flags

--- tests/vec2_geowars_test.cpp
+++ tests/vec2_geowars_test.cpp
@@ -5,6 +5,52 @@
 #include "geowars/vec2.hpp"
 
 TEST_CASE( "Vec2 template instantiation", "[vec2]" ) {
+	template <typename T>
+	void Vec2_Test(Vec2<T> test_a, Vec2<T> test_b) {
+		
+		Vec2<T> test_c = test_a + test_b;
+		CHECK( test_c.x == 3 );
+		CHECK( test_c.y == 18 );
+		
+		test_c = test_b - test_a;
+		CHECK( test_c.x == -1 );
+		CHECK( test_c.y == 2 );
+
+		test_c = test_a / test_b;
+		CHECK( test_c.x == 2 );
+		CHECK( test_c.y == 0 );
+
+		test_c = test_a * b;
+		CHECK( test_c.x == 2 );
+		CHECK( test_c.y == 80 );
+		
+		CHECK( test_a.x == test_d.x );
+		CHECK( test_a.y == test_d.y );
+		
+		CHECK( test_a.x != test_b.x );
+		CHECK( test_a.y != test_b.y );
+
+		test_c += test_b;
+		CHECK( test_c.x == 1 );
+		CHECK( test_c.y == 8 );
+
+		test_a -= test_b;
+		CHECK( test_a.x == 1 );
+		CHECK( test_a.y == -2 );
+
+		test_b *= test_a;
+		CHECK( test_b.x == 2 );
+		CHECK( test_b.y == 80 );
+
+
+
+	};
+
+	SECTION( "Vec2" ) {
+
+	}
+
+	/*
 	Vec2<int> test_a( 2, 8 );
 	Vec2<int> test_b( 1, 10 );
 	Vec2<int> test_c( 0, 0 );
@@ -35,7 +81,7 @@ TEST_CASE( "Vec2 template instantiation", "[vec2]" ) {
 		test_c = test_a * b;
 		CHECK( test_c.x == 2 );
 		CHECK( test_c.y == 80 );
-///
+		
 		CHECK( test_a.x == test_d.x );
 		CHECK( test_a.y == test_d.y );
 		
@@ -98,6 +144,7 @@ TEST_CASE( "Vec2 template instantiation", "[vec2]" ) {
 		CHECK( test_b.x == 2 );
 		CHECK( test_b.y == 0 );
 	}
+	*/
 	
 	/*	Vec2 vec2;
 	REQUIRE( vec2.x == 0 );
