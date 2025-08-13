b27b7b0b4fdaef1542641f69b513702c28bff587
alexandria <alexandria.hmann@gmail.com>
2025-08-13 15:07:28 -0500

    Updated main

--- src/main/geowars_main.cpp
+++ src/main/geowars_main.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
 
 #include "geowars/game.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {	
-	Game g("config.json");
+	std::string config_file_path = "config.json";
+	GameConfig game(config_file_path);
+	Game g(game.configInit());
 	g.run();
+
+	return 0;
 }
